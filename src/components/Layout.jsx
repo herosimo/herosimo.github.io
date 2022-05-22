7b295c362a5d99adb2e3a3ed7bf501b2e42fc84c
Herosimo Sribiko <herosimo06@gmail.com>
2022-05-22 16:13:59 +0700

    Change page title

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Helmet } from "react-helmet";
 
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
@@ -7,16 +9,33 @@ import "../styles/style.scss";
 import { layout, aside, article } from "../styles/layout.module.scss";
 
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  console.log(data);
+
   return (
-    <main className={layout}>
-      <aside className={aside}>
-        <Sidebar />
-      </aside>
-      <article className={article}>
-        {children}
-        <Footer />
-      </article>
-    </main>
+    <>
+      <Helmet>
+        <title>{data.site.siteMetadata.title}</title>
+      </Helmet>
+      <main className={layout}>
+        <aside className={aside}>
+          <Sidebar />
+        </aside>
+        <article className={article}>
+          {children}
+          <Footer />
+        </article>
+      </main>
+    </>
   );
 };
 
